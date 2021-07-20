dd838491b2a0259334a2b2323b42f97f5d5827f8
JD <40767569+jadwight@users.noreply.github.com>
2021-07-20 00:08:14 -0500

    Update Hardware-Reporter.ps1
    Implementing a function I found on Techibee. Allows for conversion of values, which can be useful in what I have planned for this script.

--- Hardware-Reporter.ps1
+++ Hardware-Reporter.ps1
@@ -1,5 +1,6 @@
 ﻿# Hardware-Reporter
 # Author: "JD" (Joshua Dwight)
+# Worth Mentioning: Borrowed Convert-Size Function from Techibee.com for Size Conversions
 # 
 # A Basic Asset Data Collector that is PowerShell-based. Purpose of program is to collect data and then dump into a simple INI-based Database.
 # A useful tool for Network Administrators looking to obtain information on computers in their network.
@@ -33,6 +34,41 @@ if ($verboseoutput -eq "1") {
     Write-Host 'Data File: ' $datadumplocation    # if verboseoutput is 1 then display output to screen
 }
 
+# Techibee Convert-Size Function
+# Source: https://techibee.com/powershell/convert-from-any-to-any-bytes-kb-mb-gb-tb-using-powershell/2376
+function Convert-Size {            
+    [cmdletbinding()]            
+    param(            
+        [validateset("Bytes","KB","MB","GB","TB")]            
+        [string]$From,            
+        [validateset("Bytes","KB","MB","GB","TB")]            
+        [string]$To,            
+        [Parameter(Mandatory=$true)]            
+        [double]$Value,            
+        [int]$Precision = 4            
+    )            
+    switch($From) {            
+        "Bytes" {$value = $Value }            
+        "KB" {$value = $Value * 1024 }            
+        "MB" {$value = $Value * 1024 * 1024}            
+        "GB" {$value = $Value * 1024 * 1024 * 1024}            
+        "TB" {$value = $Value * 1024 * 1024 * 1024 * 1024}            
+    }            
+                
+    switch ($To) {            
+        "Bytes" {return $value}            
+        "KB" {$Value = $Value/1KB}            
+        "MB" {$Value = $Value/1MB}            
+        "GB" {$Value = $Value/1GB}            
+        "TB" {$Value = $Value/1TB}            
+                
+    }            
+                
+    return [Math]::Round($value,$Precision,[MidPointRounding]::AwayFromZero)            
+                
+    }         
+# End of Techibee Function
+
 ### DATA COLLECTION ###
 
 $pcname = ($env:COMPUTERNAME) # Get Host Name
@@ -53,6 +89,8 @@ if ($verboseoutput -eq '1') {
 }
 
 # RAM Max Capacity
+$maxram = (wmic memphysical get maxcapacity)
+
 # RAM Installed
 # Hard Drive Capacity
 # Hard Drive Free Space
